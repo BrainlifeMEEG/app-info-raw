c4036f1029fdcaf184b5f169682a66c32fb54a79
Maximilien Chaumon <maximilien.chaumon@protonmail.com>
2025-09-01 16:59:53 +0200

    use read_info

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 import os
 import json
 import mne
+import numpy as np
 
 # Current path
 __location__ = os.path.realpath(
@@ -21,8 +22,7 @@ with open(__location__+'/config.json') as config_json:
 fname = config['raw']
 
 # Read the raw data and info
-raw = mne.io.read_raw_fif(fname)
-info = raw.info
+info = mne.io.read_info(fname)
 
 #Save the info into a info.txt file
 with open(os.path.join('out_dir','info.txt'), 'w') as f:
@@ -33,6 +33,17 @@ dict_json_product = {'brainlife': []}
 
 info = str(info)
 dict_json_product['brainlife'].append({'type': 'info', 'msg': info})
+
+
+positions = raw._get_channel_positions()
+if positions is not None and np.any(~np.isnan(positions)):
+    channel_positions_msg = "Channel positions:\n" + "\n".join(
+        [f"{ch_name}: {pos.tolist()}" for ch_name, pos in zip(raw.ch_names, positions)]
+    )
+    dict_json_product['brainlife'].append({'type': 'info', 'msg': channel_positions_msg})
+else:
+    dict_json_product['brainlife'].append({'type': 'info', 'msg': 'Full list of channels (no positions available): ' + ', '.join(raw.ch_names)})
+
 
 with open('product.json', 'w') as outfile:
     json.dump(dict_json_product, outfile)
